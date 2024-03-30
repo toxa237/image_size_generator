d45ebc31cc3083404cee06357c8351238c12e768
toxa237 <qwetyiop57@gmail.com>
2024-03-30 15:22:01 +0200

    fixed resizer have to work correctly

--- photo_resizer.py
+++ photo_resizer.py
@@ -12,11 +12,11 @@ class ImageResizer:
     def predict(self, img, left=0, right=0, top=0, bottom=0):
         if right:
             for i in range(right//self.size+1):
-                img = self.predict_horizontal_right(img)
+                img = self.predict_vertical_right(img)
             img = img[:, :-(self.size - right % self.size)]
         if left:
             for i in range(left//self.size+1):
-                img = self.predict_horizontal_left(img)
+                img = self.predict_vertical_left(img)
             img = img[:, (self.size - left % self.size):]
         if top:
             for i in range(top//self.size+1):
@@ -28,7 +28,7 @@ class ImageResizer:
             img = img[:-(self.size - bottom % self.size), :]
         return img
 
-    def predict_horizontal_right(self, img):
+    def predict_vertical_right(self, img):
         img = np.array(img)
         len_last_piece = img.shape[0] % self.size
         input_val = []
@@ -37,11 +37,11 @@ class ImageResizer:
         input_val.append([img[i*self.size-1:(i+1)*self.size-1, -self.size:] for i in range(-3, 0)])
         input_val = np.array(input_val)
         predict = self.model.predict(input_val)
-        predict = np.reshape(predict, (-1, self.size, 3))
+        predict = np.concatenate(predict, axis=0)
         predict = np.delete(predict, np.s_[-self.size:-len_last_piece], 0)
         return np.append(img, predict, axis=1)
 
-    def predict_horizontal_left(self, img):
+    def predict_vertical_left(self, img):
         img = np.array(img)
         len_last_piece = img.shape[0] % self.size
         input_val = []
@@ -50,7 +50,7 @@ class ImageResizer:
         input_val.append([img[i*self.size:(i+1)*self.size, :self.size] for i in range(3)][::-1])
         input_val = np.array(input_val)
         predict = self.model.predict(input_val)
-        predict = np.reshape(predict, (-1, self.size, 3))
+        predict = np.concatenate(predict, axis=0)
         predict = np.delete(predict, np.s_[-self.size:-len_last_piece], 0)
         return np.append(predict, img, axis=1)
 
@@ -60,10 +60,10 @@ class ImageResizer:
         input_val = []
         for step in range(0, img.shape[1]-self.size, self.size):
             input_val.append(np.split(img[:3*self.size, step:step+self.size], 3, 0)[::-1])
-        input_val.append([img[:self.size, i*self.size:(i+1)*self.size] for i in range(3)][::-1])
+        input_val.append(np.split(img[:3*self.size, -self.size:], 3, 0)[::-1])
         input_val = np.array(input_val)
         predict = self.model.predict(input_val)
-        predict = np.reshape(predict, (self.size, -1, 3))
+        predict = np.concatenate(predict, axis=1)
         predict = np.delete(predict, np.s_[-self.size:-len_last_piece], 1)
         return np.append(predict, img, axis=0)
 
@@ -73,10 +73,10 @@ class ImageResizer:
         input_val = []
         for step in range(0, img.shape[1]-self.size, self.size):
             input_val.append(np.split(img[-3*self.size:, step:step+self.size], 3, 0))
-        input_val.append([img[-self.size:, i*self.size-1:(i+1)*self.size-1] for i in range(-3, 0)])
+        input_val.append(np.split(img[-3*self.size:, -self.size:], 3, 0))
         input_val = np.array(input_val)
         predict = self.model.predict(input_val)
-        predict = np.reshape(predict, (self.size, -1, 3))
+        predict = np.concatenate(predict, axis=1)
         predict = np.delete(predict, np.s_[-self.size:-len_last_piece], 1)
         return np.append(img, predict, axis=0)
 
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     image = np.array(load_img('../../foto1.jpg'))/255
     print(image.shape)
     q = ImageResizer()
-    image = q.predict(image, left=200, right=200, top=50, bottom=50)
+    image = q.predict(image, bottom=110)
     print(image.shape)
     plt.imshow(image)
     plt.show()
